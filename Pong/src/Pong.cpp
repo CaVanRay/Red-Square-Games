256fb79d425fbfa7a36c15aa9e54241749d72c7c
Cavan Theiss <cavanrt@gmail.com>
2026-05-26 16:13:16 -0500

    Update Pong.cpp

--- Pong/src/Pong.cpp
+++ Pong/src/Pong.cpp
@@ -33,15 +33,9 @@ int main() {
     bool running = true;
     SDL_Event event;
 
-    SDL_Rect redSquare = {960, 400, 40, 40}; // Initial position and size of the red square
-    SDL_Rect ghostSquare = {960, 400, 40, 40}; // Ghost square for collision detection
-    SDL_Rect bluePlatform = {960, 1200, 400, 40}; // Blue platform in center of the screen
-    SDL_Rect greenPlatform = {500, 1000, 400, 40}; // Green platform on the left side of the screen
-    SDL_Rect yellowPlatform = {1100, 850, 400, 40}; // Yellow platform on the right side of the screen
+    SDL_Rect leftPaddle = {960, 1200, 400, 40}; 
+    SDL_Rect rightPaddle = {500, 1000, 400, 40};
 
-    float verticalVelocity = 0.0f; // Speed of the red square
-    const float GRAVITY = 0.2f; // Gravity affecting the red square
-    bool onGround = true; // To check if the red square is on the ground for jumping
 
     // **************************************** GAME LOOP **************************************** 
 
